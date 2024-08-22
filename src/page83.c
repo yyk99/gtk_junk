ba3a6e69f4ed88ec172f7ed80188c034a7dbe8dc
Yura K <yyk@silvana.home>
2024-08-21 20:52:14 -0400

    Add delete_event sihnal

--- src/page83.c
+++ src/page83.c
@@ -13,6 +13,7 @@ void main (int argc, char *argv[])
 	GtkWidget *win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	GtkWidget *btn = gtk_button_new_with_label ("Close window");
 	g_signal_connect (btn, "clicked", G_CALLBACK (end_program), NULL);
+	g_signal_connect (win, "delete_event", G_CALLBACK (end_program), NULL);
 	gtk_container_add (GTK_CONTAINER (win), btn);
 	gtk_widget_show_all (win);
 	gtk_main ();
